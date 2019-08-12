63f572536b32f2401faec26b2301f745632bbedb
Sourav Tosh <marvel_magnum@yahoo.com>
2019-08-12 02:56:11 -0300

    Ships

--- gamestate.py
+++ gamestate.py
@@ -1,11 +1,32 @@
 import buildings
-
+import ships
 
 class GameState:
     offers = {'money': 2, 'fish': 2, 'wood': 2, 'clay': 1, 'iron': 0, 'grain': 0, 'cattle': 0}
     inventory = {'money': 5, 'coal': 1, 'wood': 10}
     current_player = 'p1'  # current player: p1, p2, p3, ...
     location = {'p1': 'none'}  # player's location: none, building names
+    ships = [{'game':[ships.wship1,
+                      ships.wship2,
+                      ships.wship3,
+                      ships.wship4,
+                      ships.wship5,
+                      ships.wship6,
+                      ships.wship7,
+                      ships.iship1,
+                      ships.iship2,
+                      ships.iship3,
+                      ships.iship4,
+                      ships.iship5,
+                      ships.iship6,
+                      ships.sship1,
+                      ships.sship2,
+                      ships.sship3,
+                      ships.sship4,
+                      ships.lship1,
+                      ships.lship2,
+                      ships.lship3],
+                'p1':[]}]
 
     bases = [['money', 'wood'],
              ['wood', 'fish', 'interest'],
